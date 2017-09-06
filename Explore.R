f29a1d7cbb3d851bcf7aa99cfb5751b18033a607
Jim Sheldon <jimsheldon@icloud.com>
2017-09-06 14:47:08 -0500

    Added code to generate school locations on a map

--- Explore.R
+++ Explore.R
@@ -1,5 +1,8 @@
 library(tidyverse)
 library(readxl)
+library(leaflet)
+library(RColorBrewer)
+library(htmlwidgets)
 
 data <- read_csv("Data/Most-Recent-Cohorts-All-Data-Elements.csv", col_types = cols(.default = 'c'))
 dict <- read_excel("Data/CollegeScorecardDataDictionary.xlsx", sheet = "data_dictionary")
@@ -10,3 +13,12 @@ dbl_cols <- dict %>% filter(`API data type` == "float") %>% select(`VARIABLE NAM
 data <- data %>% mutate_at(vars(which(names(data) %in% int_cols[[1]])), as.numeric) %>% 
     mutate_at(vars(which(names(data) %in% dbl_cols[[1]])), as.double)
 
+
+# Map the School Locations ------------------------------------------------
+
+
+leaflet() %>% addProviderTiles(providers$CartoDB.Positron) %>% 
+    addMarkers(data=data, lat=~LATITUDE, lng=~LONGITUDE, label = ~INSTNM, 
+               clusterOptions=markerClusterOptions())
+
+saveWidget(m, file='schools.html')
